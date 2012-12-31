8d325f6c502702042269a45062dacfb17981e7f6
Prajwal A N <prajwal.an92@gmail.com>
2012-12-31 09:22:38 +0530

    Basic functionality

---
+++ README.md
@@ -0,0 +1,27 @@
+Mark-It
+=======
+Mark articles for later reading...
+
+---
+This chrome extension helps you to keep track of articles that you may want to read later but find them not that important to be bookmarked. 
+
+Usage
+-----
+1. To save a particular article, click the extension button when the tab having the article is selected, and click "Add Articles" button. In the dialog box that appears, click "Choose title" to name the article with the title of the article itself, else choose "Save Custom Title" button after entering the desired title of the article.
+2. To delete all the articles, click on "Clear" button.
+
+Requirements
+------------
+This extension uses the chrome.storage api and the tab api. It does not save your history or does not access anything other than the data that you have stored while using this extension (more precisely the articles that you have marked).
+
+Installation
+------------
+To use this extension follow the steps given below.
+
+1. Download the source code.
+2. Extract the folder.
+3. Go to chrome://chrome/extensions/
+4. Select the "Developer mode" option if it has not yet been selected.
+5. Then click on "Load unpacked extension...".
+6. Go to the extracted directory in the dialog box that appears and click open.
+7. Select "Allow in incognito mode" if you want the extension in incognito mode as well (see Requirements section above).
---
+++ popup.js
@@ -0,0 +1,92 @@
+var storagearea = chrome.storage.local;
+
+var titleButton = document.querySelector('#titlebutton');
+titleButton.addEventListener('click', addArticlesTitle);
+var customtitleButton = document.querySelector('#customtitlebutton');
+customtitleButton.addEventListener('click', addArticlesCustomTitle);
+var delButton = document.querySelector('#dbutton');
+delButton.addEventListener('click', purgeArticles);
+
+var message = document.querySelector('#articlespace');
+var length=0;
+var curr_articles;
+var imagestr = '<img src="./img/bullet.png" class="image"></img> ';
+var purgemsg = 'All articles successfully purged!!';
+var noartmsg = 'No articles marked yet!!';
+var donemsg = "Article added!!";
+var alertclose = '<button type="button" class="close" data-dismiss="alert">×</button>';
+
+function loadarticles(){
+  storagearea.get('articles', function(items){
+    //alert(items.articles);
+    if(items.articles){
+      curr_articles = items.articles;
+      length = items.articles.length;
+      for(i=0;i<length;++i){
+        message.innerHTML += imagestr+items.articles[i]+'<br>';
+      }
+    } 
+    else{
+      var optionsUrl = chrome.extension.getURL('options.html');
+      message.innerHTML += noartmsg;
+    }
+  });
+}
+
+
+function addArticles(ttitle){
+  chrome.tabs.getSelected(null,function(tab){
+    var addurl = '<a href="'+tab.url+'">'+ttitle+'</a>';
+    if(length > 0){
+      curr_articles[length] = addurl;
+      length++;
+    }
+    else{
+      curr_articles = [];
+      curr_articles[0] = addurl;
+    } 
+    storagearea.set({'articles': curr_articles},function(){
+      //document.querySelector('#alertmessages').innerHTML = donemsg+alertclose;
+      message.innerHTML += ' ';
+    });
+    if(length == 1){
+      message.innerHTML = imagestr+addurl+"<br>";
+    }
+    else{
+      if(message.innerHTML == noartmsg || message.innerHTML == purgemsg)
+        message.innerHTML = imagestr+addurl+"<br>";
+      else
+        message.innerHTML += imagestr+addurl+"<br>";
+    }
+    //window.console.log('<a href="'+tab.url+'">'+tab.title+'</a>');
+  });
+}
+
+
+function addArticlesTitle(){
+  chrome.tabs.getSelected(null,function(tab){
+    addArticles(tab.title);
+  });
+}
+
+
+function addArticlesCustomTitle(){
+  if(document.querySelector('#customtitle').value){
+    addArticles(document.querySelector('#customtitle').value);
+  }
+  else{
+    chrome.tabs.getSelected(null,function(tab){
+      addArticles(tab.title);
+    });
+  }
+}
+
+
+function purgeArticles(){
+  storagearea.remove('articles', function(items) {
+    message.innerHTML = noartmsg;
+    //document.querySelector('#alertmessages').innerHTML = noartmsg+alertclose;
+  });
+}
+
+loadarticles();
